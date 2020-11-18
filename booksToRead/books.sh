c3325e12a5a1a6fb33c101ccc1e4aca47803e97d
James Njuguna <jameskinyua590@gmail.com>
2020-11-18 17:11:46 +0300

    create a shortcut for frequently-read books

---
+++ booksToRead/books.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+books_path=Downloads/books
+
+openSolidBook() {
+  open "${HOME}/$books_path/software_development/system_design/solid-software-design-architecture-handbook.epub"
+}
+
+openBashPocketBook() {
+  open "${HOME}/$books_path/operating_systems/unix/bash/bash-command-line-shell-scripts-pocket.pdf"
+}
+
