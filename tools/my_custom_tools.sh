6adbf6a3322da606aa839b32a6e1125820f3a991
James Njuguna <jameskinyua590@gmail.com>
2020-11-20 11:53:26 +0300

    create a tool for searching text in a file

--- tools/my_custom_tools.sh
+++ tools/my_custom_tools.sh
@@ -1,10 +1,34 @@
 #!/usr/bin/env bash
 
 findThisFile() {
-  find . -iregex $1 -type $2 -print
+  fileType=$2
+
+  if [[ -z $fileType ]];
+  then
+	find . -iregex $1 -type f -print
+  else
+	find . -iregex $1 -type d -print
+  fi
 }
 
 makeDirectoryAndEnter() {
-  mkdir $1 && cd $_
+  name=$1
+
+  if [[ -n $name ]];
+  then
+	mkdir $name && cd $_
+  else
+	echo "Please provide a directory name"
+  fi
 }
 
+searchForThisPhrase() {
+  pattern=$1
+
+  if [[ -n $pattern ]];
+  then
+	grep -inE $pattern
+  else
+	echo "Please provide a regex pattern to use in the search"
+  fi
+}
