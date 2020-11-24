13c302fcb0510ccbf4ebca82c7601a11746ca784
James Njuguna <jameskinyua590@gmail.com>
2020-11-24 21:29:39 +0300

    create a convenience_folder variable

--- myGotos.sh
+++ myGotos.sh
@@ -1,13 +1,16 @@
-alias goToMaxNode="source ${HOME}/Desktop/convenience/maxim_node/setup_workspace_for_maximillian_node.sh"
-alias goToStephMicroservices="source ${HOME}/Desktop/convenience/stephgrider_microservices/setup_workspace_for_stephmicroservices.sh"
-alias goToAlgorithms="source ${HOME}/Desktop/convenience/algorithms_and_ds/setup_workspace_for_algorithms_and_ds.sh"
-alias gotoBooks="cd $HOME/Downloads/books"
+convenience_folder="$HOME/Desktop/convenience"
+
+alias goToMaxNode="source $convenience_folder/maxim_node/setup_workspace_for_maximillian_node.sh"
+alias goToStephMicroservices="source $convenience_folder/stephgrider_microservices/setup_workspace_for_stephmicroservices.sh"
+alias goToAlgorithms="source $convenience_folder/algorithms_and_ds/setup_workspace_for_algorithms_and_ds.sh"
+alias goToBooks="cd $HOME/Downloads/books"
+alias goToSolidBook="source $convenience_folder/solid_architecture/setup_solid_workspace.sh"
 
 # my custom tools
-source "${HOME}/Desktop/convenience/tools/my_custom_tools.sh"
+source "$convenience_folder/tools/my_custom_tools.sh"
 
 # my frequently-read books
-source "${HOME}/Desktop/convenience/booksToRead/books.sh"
+source "$convenience_folder/booksToRead/books.sh"
 
 # my custom env variables
 export PERSONAL_LEARNING="${HOME}/Desktop/personal_learning"
