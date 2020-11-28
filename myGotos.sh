8fe0ac7a2a19bde750734782130df94b156ba770
James Njuguna <jameskinyua590@gmail.com>
2020-11-29 00:52:29 +0300

    rename and export path to convenience dir

--- myGotos.sh
+++ myGotos.sh
@@ -1,17 +1,18 @@
-convenience_folder="$HOME/Desktop/convenience"
+# my custom env variables
+export PERSONAL_LEARNING="$HOME/Desktop/personal_learning"
+export BOOKS_PATH="$HOME/Downloads/books"
+export CONVENIENCE_DIR="$HOME/Desktop/convenience"
 
-alias goToMaxNode="source $convenience_folder/maxim_node/setup_workspace_for_maximillian_node.sh"
-alias goToStephMicroservices="source $convenience_folder/stephgrider_microservices/setup_workspace_for_stephmicroservices.sh"
-alias goToAlgorithms="source $convenience_folder/algorithms_and_ds/setup_workspace_for_algorithms_and_ds.sh"
+# my custom aliases for frequently visited projects
+alias goToMaxNode="source $CONVENIENCE_DIR/maxim_node/setup_workspace_for_maximillian_node.sh"
+alias goToStephMicroservices="source $CONVENIENCE_DIR/stephgrider_microservices/setup_workspace_for_stephmicroservices.sh"
+alias goToAlgorithms="source $CONVENIENCE_DIR/algorithms_and_ds/setup_workspace_for_algorithms_and_ds.sh"
 alias goToBooks="cd $HOME/Downloads/books"
-alias goToSolidBook="source $convenience_folder/solid_architecture/setup_solid_workspace.sh"
+alias goToSolidBook="source $CONVENIENCE_DIR/solid_architecture/setup_solid_workspace.sh"
 
 # my custom tools
-source "$convenience_folder/tools/my_custom_tools.sh"
+source "$CONVENIENCE_DIR/tools/my_custom_tools.sh"
 
 # my frequently-read books
-source "$convenience_folder/booksToRead/books.sh"
+source "$CONVENIENCE_DIR/booksToRead/books.sh"
 
-# my custom env variables
-export PERSONAL_LEARNING="${HOME}/Desktop/personal_learning"
-export BOOKS_PATH="${HOME}/Downloads/books"
